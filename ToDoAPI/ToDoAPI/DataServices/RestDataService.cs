94bfec4cb75b3b5972c38e00ffdb7b5d3a16c775
Ingvar Derešivski <inde999@gmail.com>
2022-12-29 20:49:55 +0200

    ServiceClassConstructor

---
+++ ToDoAPI/ToDoAPI/DataServices/RestDataService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ToDoApi.Models;
+
+namespace ToDoApi.DataServices
+{
+    public class RestDataService : IRestDataService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseAddress;
+        private readonly string _url;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public RestDataService
+            (
+
+            )
+        {
+            _httpClient = new HttpClient();
+            _baseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.2.2:5209" : "https://localhost:7209";
+            _url = $"{_baseAddress}/api";
+            _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+        }
+
+
+
+        public Task AddTodoAsync(ToDo toDo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTodoAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ToDo>> GetAllToDosAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTodoAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
