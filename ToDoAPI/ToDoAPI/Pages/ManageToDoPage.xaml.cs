50ac93d712156959983ca1ce998e705b22335924
Ingvar Derešivski <inde999@gmail.com>
2023-01-02 13:36:46 +0200

    QueryProperty

--- ToDoAPI/ToDoAPI/Pages/ManageToDoPage.xaml.cs
+++ ToDoAPI/ToDoAPI/Pages/ManageToDoPage.xaml.cs
@@ -1,8 +1,11 @@
+//using Android.OS;
+using System.Diagnostics;
 using ToDoApi.DataServices;
 using ToDoApi.Models;
 
 namespace ToDoApi.Pages;
 
+[QueryProperty(nameof(ToDoApi.Models.ToDo), "ToDo")]
 public partial class ManageToDoPage : ContentPage
 {
 	private readonly IRestDataService _dataService;
@@ -36,5 +39,32 @@ public partial class ManageToDoPage : ContentPage
 		}
 
 		return false;
+	}
+
+	async void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+		if (_isNew)
+		{
+			Debug.WriteLine("--> Add new Item");
+			await _dataService.AddTodoAsync(ToDo);
+		}
+		else
+		{
+            Debug.WriteLine("--> Update an Item");
+            await _dataService.AddTodoAsync(ToDo);
+        }
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+	async void OnDeleteButtonClicked(object sender, EventArgs e)
+	{
+		await _dataService.DeleteTodoAsync(ToDo.Id);
+        await Shell.Current.GoToAsync("..");
+    }
+
+	async void OnCancelButtonCliked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("..");
 	}
 }
